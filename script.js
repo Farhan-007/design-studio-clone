47f836654a0e0e56a2da307bd083e770a6cfd4a4
Farhan akhtar <sheikhfarhan722@gmail.com>
2023-12-05 17:01:35 +0530

    added loader

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const container = document.querySelector('.container')
 const cards = gsap.utils.toArray('.container .card')
 
+
+//loader
+
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('contents').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('contents').style.visibility="visible";
+        },1000);
+    }
+  }
+
+//horizontal scroll cards
 let scrollTween = gsap.to(cards, {
     xPercent: -100 * (cards.length - 1),
     ease: "none",
